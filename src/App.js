09d579e49a56b876b93eaa4bcefba3fda268763f
Liz Verde <>
2017-12-24 12:41:44 +0000

    Following along with the Form validation demo for react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>React Form Validation Demo</h2>
+        </div>
+        <Form />
       </div>
     );
   }
